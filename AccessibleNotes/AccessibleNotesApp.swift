4289906f9aef0d1395d64666c1b2b8a4cb20870a
Mandar Gondane <mandargondane@gmail.com>
2026-03-27 12:58:32 -0700

    Initial Commit

---
+++ AccessibleNotes/AccessibleNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibleNotesApp.swift
+//  AccessibleNotes
+//
+//  Created by Mandar Gondane on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct AccessibleNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
